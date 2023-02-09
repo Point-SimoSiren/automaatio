a857f3ee5b6647a1d69b3bda32c51fa33598cd87
unknown <sirensimo7@gmail.com>
2023-02-09 20:11:36 +0200

    jpedwoj

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'node': true,
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+        2
+      ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "error",
+    "react/prop-types": 0,
+    
+    // Added 29.1.2022 - Simo
+    "no-console": "off",
+    "no-restricted-syntax": [
+        "error",
+        {
+            "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+            "message": "Unexpected property on console object was called"
+        }
+    ]
+    
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Kommentti
+import './styles.css'
+import App from './App'
+
+//import './styles.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
